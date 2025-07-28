757e2deb282cb273ef33ecb9cbf986727146dd0f
classikCherryWood <shih-hao.chan@bayes.city.ac.uk>
2025-07-28 19:49:08 +0100

    text processing methods textToString & makeWordDict

--- stylometry.py
+++ stylometry.py
@@ -9,7 +9,14 @@ def textToString(filename):
     """read a text file and return a string"""
     with open(filename) as f: 
         return f.read()
-    
+
+def makeWordDict(stringsByAuthor):
+    """return dictionary of tokenised workds by corpus by author"""
+    wordsByAuthor = dict()
+    for author in stringsByAuthor:
+        tokens = nltk.word_tokenize(stringsByAuthor[author])
+        wordsByAuthor[author] = ([token.lower() for token in tokens
+                                  if token.isalpha()])
     
 if __name__ == "__main__":
     pass
